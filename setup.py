10f57d5e61352f209d76b8acaaff0374f4427ca7
David Stuebe <stu3b3@gmail.com>
2020-04-01 13:32:39 -0400

    Bump version to test ci

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="oktadboto",
-    version="0.0.2",
+    version="0.0.3",
     author="Upserve",
     author_email="datascience@upserve.com",
     description="Boto3 RefreshableCredentials wrapping oktad system call",
